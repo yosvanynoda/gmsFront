2f1963e7e75baf617e3c348e53586004256e29f7
yosvanynoda <yosvanynoda@yahoo.com>
2025-10-10 23:07:51 -0400

    yn Study UI

---
+++ GMS_UI/Models/Enum/ArmEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace GMS_UI.Models.Enum
+{
+    public enum ArmEnum
+    {
+        //Right, left y both
+
+        Right = 1,
+
+        Left = 2,
+
+        Both = 3,
+    }
+}
--- GMS_UI/Models/STD/STDArmsViewModel.cs
+++ GMS_UI/Models/STD/STDArmsViewModel.cs
@@ -2,5 +2,14 @@
 {
     public class STDArmsViewModel
     {
+        public int ArmID { get; set; } = 0;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public int TargetEnrollment { get; set; } = 0;
+
+        public string DoseLevel { get; set; } = string.Empty;
     }
 }
